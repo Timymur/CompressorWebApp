be1acb7f8503306671a019cf9c835ebcdf278340
Timymur <hitechmag2013@mail.ru>
2025-11-25 14:22:05 +0500

    second commit

---
+++ src/main/java/com/example/CompressorWebApp/CompressorWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.CompressorWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompressorWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompressorWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CompressorWebApp/CompressorWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CompressorWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompressorWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
